654a11a327d6de50083ddd51b2fa9ba63e36ee86
Nicholas March <n.march213@gmail.com>
2017-08-08 03:32:02 -0500

    Added User Model

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const keys = require('./config/keys');
 require('./services/passport'); //This is so the file is ran.
+require('./models/User');
+
+mongoose.connect(keys.mongoURI);
 
 const app = express();
 
