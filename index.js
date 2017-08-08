29a8933d3e85d0cca167887f1a5e098d8afcefcf
Nicholas March <n.march213@gmail.com>
2017-08-08 05:03:43 -0500

    Authenticate with cookies working

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
 const keys = require('./config/keys');
 require('./models/User'); //Have to place before passport to instantiate userSchema
 require('./services/passport'); //This is so the file is ran.
@@ -7,6 +9,16 @@ require('./services/passport'); //This is so the file is ran.
 mongoose.connect(keys.mongoURI);
 
 const app = express();
+
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKey]
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 // Routes
 require('./routes/authRoutes')(app);
