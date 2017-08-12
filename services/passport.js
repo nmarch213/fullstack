25883483f55e8fc2191f63e25304fae44f51b6b3
Nicholas March <n.march213@gmail.com>
2017-08-12 01:25:51 -0500

    Async refactor

--- services/passport.js
+++ services/passport.js
@@ -23,19 +23,16 @@ passport.use(
       callbackURL: '/auth/google/callback',
       proxy: true
     },
-    (accessToken, refreshToken, profile, done) => {
+    async (accessToken, refreshToken, profile, done) => {
       // The then statement is a promise that will run once the mongodb has ran
-      User.findOne({ googleId: profile.id }).then(existingUser => {
-        if (existingUser) {
-          // Already have a record with this userSchema
-          done(null, existingUser);
-        } else {
-          // Only creates new user if not previously added
-          new User({ googleId: profile.id })
-            .save()
-            .then(user => done(null, user));
-        }
-      });
+      const existingUser = await User.findOne({ googleId: profile.id });
+      if (existingUser) {
+        // Already have a record with this userSchema
+        return done(null, existingUser);
+      }
+      // Only creates new user if not previously added
+      const user = await new User({ googleId: profile.id }).save();
+      done(null, user);
     }
   )
 );
