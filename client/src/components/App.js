b475cb85e321d86d1d006cdaa639a3a1f6549fe5
Nicholas March <n.march213@gmail.com>
2017-08-12 21:27:27 -0500

    Refactor App to accept actions

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -7,19 +7,23 @@ const Dashboard = () => <h2>Dashboard</h2>;
 const SurveyNew = () => <h2>SurveyNew</h2>;
 const Landing = () => <h2>Landing</h2>;
 
-const App = () => {
-  return (
-    <div>
-      <BrowserRouter>
-        <div className="container">
-          <Header />
-          <Route exact path="/" component={Landing} />
-          <Route exact path="/surveys" component={Dashboard} />
-          <Route path="/surveys/new" component={SurveyNew} />
-        </div>
-      </BrowserRouter>
-    </div>
-  );
-};
+class App extends Component {
+  componentDidMount() {}
+
+  render() {
+    return (
+      <div>
+        <BrowserRouter>
+          <div className="container">
+            <Header />
+            <Route exact path="/" component={Landing} />
+            <Route exact path="/surveys" component={Dashboard} />
+            <Route path="/surveys/new" component={SurveyNew} />
+          </div>
+        </BrowserRouter>
+      </div>
+    );
+  }
+}
 
 export default App;
