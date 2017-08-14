883c5c0dc30ae7e2a5f9d08b10cb9fc3b327f757
Nicholas March <n.march213@gmail.com>
2017-08-13 23:40:41 -0500

    Adding Auth recognition to header

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,17 +1,33 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 class Header extends Component {
+  renderContent() {
+    switch (this.props.auth) {
+      case null:
+        return;
+      case false:
+        return (
+          <li>
+            <a href="/auth/google">Login With Google</a>
+          </li>
+        );
+      default:
+        return (
+          <li>
+            <a>Logout</a>
+          </li>
+        );
+    }
+  }
+
   render() {
     return (
       <nav>
         <div className="nav-wrapper">
           <a className="left brand-logo">Emaily</a>
           <ul className="right">
-            <li>
-              <a>Login With Google</a>
-            </li>
-            <li />
-            <li />
+            {this.renderContent()}
           </ul>
         </div>
       </nav>
@@ -19,4 +35,8 @@ class Header extends Component {
   }
 }
 
-export default Header;
+function mapStateToProps({ auth }) {
+  return { auth };
+}
+
+export default connect(mapStateToProps)(Header);
